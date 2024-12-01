d96f1dd117216b27cb436ce644b28b68c4a0f825
anushka bhandari <bhandarianushka670@gmail.com>
2024-12-02 01:40:59 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Your backend server
+        target: 'https://qna-finance-1.onrender.com/', // Your backend server
         changeOrigin: true,
         secure: false,
       },
