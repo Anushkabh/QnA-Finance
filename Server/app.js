76dd279871312f48831a2950c561773aa356a18d
anushka bhandari <bhandarianushka670@gmail.com>
2024-12-05 00:51:06 +0530

    bcd

--- Server/app.js
+++ Server/app.js
@@ -30,6 +30,12 @@ app.use(cors({
     ], 
 }));
 
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/question", questionRouter);
